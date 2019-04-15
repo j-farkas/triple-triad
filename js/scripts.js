9b0d7432025c071c84ed45326b5ef0b7d306829f
Student <example@example.com>
2019-04-15 10:59:42 -0700

    add clickable cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-Function Game(){
+function Game(){
   this.deck = [],
   this.score = 5,
   this.player1 = new Player(),
@@ -6,17 +6,18 @@ Function Game(){
   this.player2 = new Player(),
   this.turn = 0,
   this.currentID = 1,
-  this.board = [0,1,2,3,4,5,6,7,8]
+  this.board = [0,1,2,3,4,5,6,7,8],
+  this.selected = false;
 }
 
 var game = new Game();
 
-Function Player(){
+function Player(){
   this.hand = [],
   this.active = false
 }
 
-Function Card(top, bottom, left, right){
+function Card(top, bottom, left, right){
   this.top = top,
   this.bottom = bottom,
   this.left = left,
@@ -26,8 +27,24 @@ Function Card(top, bottom, left, right){
   game.deck.push(this);
 }
 
-var selected = false;
+
+function attachListeners() {
+  $("body").on("click", "img.card", function() {
+    game.selected = this.id;
+  });
+  $(".container").on("click", ".col-md-4", function() {
+    if(game.selected !== false){
+      var location = $(this).attr('class');
+      location = location.charAt(location.length-1);
+      console.log(location);
+      $("img."+location).attr("src","img/"+game.selected+"_b.png");
+      $("#"+game.selected).remove();
+      game.selected = false;
+      console.log(game.selected);
+    }
+  });
+};
 
 $(document).ready(function() {
-  //attachListeners();
-})
+  attachListeners();
+});
